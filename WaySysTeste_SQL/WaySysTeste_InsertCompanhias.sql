96bb7756326215915caa6ca76ca363a9ed4325d9
RUY JUNIOR <63271763+ruyjunior@users.noreply.github.com>
2024-12-10 10:13:26 -0300

    Delete WaySysTeste_SQL directory

--- WaySysTeste_SQL/WaySysTeste_InsertCompanhias.sql
+++
@@ -1,6 +0,0 @@
-describe  companhiasaereas;
-INSERT INTO companhiasaereas (CodCompanhia, Nome) VALUES ('1', 'Azul');
-INSERT INTO companhiasaereas (CodCompanhia, Nome) VALUES ('2', 'Gol');
-INSERT INTO companhiasaereas (CodCompanhia, Nome) VALUES ('3', 'Latam');
-INSERT INTO companhiasaereas (CodCompanhia, Nome) VALUES ('4', 'Avianca');
-select * from companhiasaereas;
